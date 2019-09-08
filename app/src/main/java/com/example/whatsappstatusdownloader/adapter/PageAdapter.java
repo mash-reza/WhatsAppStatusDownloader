bf247c68ac89998df9a5358664e5b35927ea7901
Festive <27864684+mash-reza@users.noreply.github.com>
2019-09-08 10:47:15 +0430

    tablayout works fine with dummy fragments

--- app/src/main/java/com/example/whatsappstatusdownloader/adapter/PageAdapter.java
+++ app/src/main/java/com/example/whatsappstatusdownloader/adapter/PageAdapter.java
@@ -1,4 +1,34 @@
 package com.example.whatsappstatusdownloader.adapter;
 
-public class PageAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.whatsappstatusdownloader.view.fragment.CachedStatus;
+import com.example.whatsappstatusdownloader.view.fragment.GalleryStatus;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private int numberOfTabs;
+
+    public PageAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.numberOfTabs = numberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i) {
+            case 0:
+                return new CachedStatus();
+            case 1:
+                return new GalleryStatus();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfTabs;
+    }
 }
--- app/src/main/java/com/example/whatsappstatusdownloader/view/activity/StatusTab.java
+++ app/src/main/java/com/example/whatsappstatusdownloader/view/activity/StatusTab.java
@@ -1,15 +1,41 @@
 package com.example.whatsappstatusdownloader.view.activity;
 
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.whatsappstatusdownloader.R;
+import com.example.whatsappstatusdownloader.adapter.PageAdapter;
 
 public class StatusTab extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    TabItem tabItem1;
+    TabItem tabItem2;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_tab);
+
+        //init ui
+        toolbar = findViewById(R.id.toolbar);
+        tabLayout = findViewById(R.id.tabLayout);
+        tabItem1 = findViewById(R.id.cachedTabItem);
+        tabItem2 = findViewById(R.id.galleryTabItem);
+        viewPager = findViewById(R.id.viewPager);
+
+
+        //setting viewpager
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount()));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
     }
 }
