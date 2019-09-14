285e9fe187ef5fef9836938fc626b0c1da4e9859
Festive <27864684+mash-reza@users.noreply.github.com>
2019-09-14 10:38:27 +0430

    download button resized

--- app/src/main/java/com/example/whatsappstatusdownloader/view/activity/Status.java
+++ app/src/main/java/com/example/whatsappstatusdownloader/view/activity/Status.java
@@ -106,7 +106,7 @@ public class Status extends AppCompatActivity {
                 Log.i(TAG, "onCreate: share button clicked");
                 Intent shareIntent = new Intent();
                 shareIntent.setAction(Intent.ACTION_SEND);
-                shareIntent.putExtra(Intent.EXTRA_STREAM, new File(path));
+                shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path));
                 shareIntent.setType("image/jpg");
                 startActivity(Intent.createChooser(shareIntent, getResources().getString(R.string.share_intent_title)));
 
